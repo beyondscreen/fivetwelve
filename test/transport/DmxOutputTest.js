7dedbd7335ad3b0ce83739449a5bf611a376e37b
Martin Schuhfuss <m.schuhfuss@gmail.com>
2016-12-03 13:52:14 +0100

    refactor(dmx-output): multi-universe support
     - implement support for multiple universes from a single output
     - fixes behaviour regarding promises returned from drivers
     - use intervals instead of FPS for start-method (breaking)
    
    BREAKING CHANGE:
        output.start() will now accept the interval and not the framerate as
        a parameter. This is probably a more expected behaviour as all
        javascript timing-related functions operate on timeouts, not
        frequencies.

--- test/transport/DmxOutputTest.js
+++ test/transport/DmxOutputTest.js
@@ -1,19 +1,29 @@
 import sinon from 'sinon';
 import expect from 'expect.js';
 
+import PromisePolyfill from 'promise-polyfill';
+
 import DmxOutput from '../../lib/transport/DmxOutput';
+
+const OrigPromise = Promise;
 
 describe('DMXOutput', () => {
   let clock, driver;
 
   beforeEach(() => {
+    // as we are testing stuff related to promises and their timing behaviour
+    // together with Promises, we want to use a promise implementation that
+    // uses the fake-timers from sinon.
+    global.Promise = PromisePolyfill;
     clock = sinon.useFakeTimers();
     driver = {
       send: sinon.spy()
     };
+    PromisePolyfill._setImmediateFn(clock.setImmediate);
   });
 
   afterEach(() => {
+    global.Promise = OrigPromise;
     clock.restore();
   });
 
@@ -32,17 +42,21 @@ describe('DMXOutput', () => {
     it('will start sending frames with the given framerate', () => {
       let out = new DmxOutput(driver);
 
-      out.start(10);
-      clock.tick(1001);
-      expect(driver.send.callCount).to.be(10);
+      // test calls when running
+      out.start(1000 / 10);
+      clock.tick(1000);
+      expect(driver.send.callCount).to.be(11);
 
+
+      // no more calls when stopped
       out.stop();
-      clock.tick(1001);
-      expect(driver.send.callCount).to.be(10);
+      clock.tick(1000);
+      expect(driver.send.callCount).to.be(11);
 
-      out.start(20);
-      clock.tick(501);
-      expect(driver.send.callCount).to.be(20);
+      // restart with diffeerent speed, make sure this stays
+      out.start(1000 / 20);
+      clock.tick(500); // 20 FPS, so this adds another 11 calls
+      expect(driver.send.callCount).to.be(22);
 
       driver.send.args.forEach(([buffer]) => {
         expect(buffer).to.be(out.getBuffer());
@@ -57,18 +71,127 @@ describe('DMXOutput', () => {
       expect(driver.send.callCount).to.be(0);
     });
 
+    it('send-loop stalls when driver-promise isn\'t resolved in time', () => {
+      driver = {
+        resolver: null,
+        spy: sinon.spy(),
+        send() {
+          this.spy();
+          return new Promise(res => { this.resolver = res; });
+        }
+      };
 
-    it('will send a single frame when send is called', () => {
+      let out = new DmxOutput(driver);
+      out.start(1000 / 10);
+
+      expect(driver.spy.callCount).to.be(0);
+
+      // the first call happens immediately
+      clock.tick(1);
+      expect(driver.spy.callCount).to.be(1);
+      // the resolver of the first call resolves after 110ms, so 10 ms too late
+      setTimeout(driver.resolver, 110);
+
+      // time = 100: as the promise didn't resolve at this point,
+      // send shouldn't have been called again yet.
+      clock.tick(99);
+      expect(driver.spy.callCount).to.be(1);
+
+      // time = 110; at this point the promise resolves (i.e. send complete),
+      // should notify about the skipped frame but not call send again.
+      clock.tick(10);
+      expect(driver.spy.callCount).to.be(1);
+
+      // time = 200; the second frame is now rendered
+      clock.tick(90);
+      expect(driver.spy.callCount).to.be(2);
+
+      driver.resolver();
+
+      // as the promise resolves immediately, we can expect the third frame to
+      // be rendered in time
+      clock.tick(100);
+      expect(driver.spy.callCount).to.be(3);
+    });
+  });
+
+  describe('send()', () => {
+    describe('multiple universes', () => {
+      let multiDriver;
+
+      beforeEach(() => {
+        multiDriver = {
+          resolvers: [],
+          promises: [],
+          spies: [sinon.spy(), sinon.spy()],
+
+          send(buffer, universe) {
+            this.spies[universe - 1](buffer, universe);
+            return this.promises[universe - 1];
+          }
+        };
+
+        multiDriver.promises.push(new Promise(resolve => { multiDriver.resolvers[0] = resolve; }));
+        multiDriver.promises.push(new Promise(resolve => { multiDriver.resolvers[1] = resolve; }));
+      });
+
+      it('calls send for every registered universe', () => {
+        let out = new DmxOutput(multiDriver, 2);
+
+        out.send();
+
+        expect(multiDriver.spies[0].callCount).to.be(1);
+        expect(multiDriver.spies[1].callCount).to.be(1);
+        expect(multiDriver.spies[0].args[0][0]).to.be(out.getBuffer(1));
+        expect(multiDriver.spies[0].args[0][1]).to.be(1);
+        expect(multiDriver.spies[1].args[0][0]).to.be(out.getBuffer(2));
+        expect(multiDriver.spies[1].args[0][1]).to.be(2);
+      });
+
+      it('returned promise resolves when all driver promises are resolved', done => {
+        let out = new DmxOutput(multiDriver, 2);
+
+        const promise = out.send();
+
+        multiDriver.resolvers[0]();
+
+        promise.then(() => done(new Error('promise shouldn\'t be resolved here')));
+
+        process.nextTick(done);
+      });
+
+      it('returned promise resolves when all driver promises are resolved', done => {
+        let out = new DmxOutput(multiDriver, 2);
+
+        const promise = out.send();
+
+        promise.then(() => done());
+
+        multiDriver.resolvers[0]();
+        multiDriver.resolvers[1]();
+
+        clock.tick(1);
+      });
+
+      it('returned promise resolves when driver doesn\'t return a promise', done => {
+        let out = new DmxOutput({send() {}});
+        out.send().then(() => { done(); });
+
+        clock.tick(0);
+      });
+    });
+  });
+
+  describe('getBuffer()', () => {
+    it('returns a valid buffer', () => {
       let out = new DmxOutput(driver);
 
-      out.start();
-      clock.tick(1000);
-      expect(driver.send.callCount).to.be(0);
+      expect(out.getBuffer()).to.be.a(Buffer);
     });
   });
 
   describe('requestDmxFrame()', () => {
-    it('will be called before the frame gets sent', () => {
+    it('callback is invoked before the frame gets sent', () => {
       let out = new DmxOutput(driver),
         spy = sinon.spy();
 
@@ -78,16 +201,18 @@ describe('DMXOutput', () => {
       };
 
       out.requestDmxFrame(callback);
-      out.start(10);
+      out.start(1000 / 10);
 
       clock.tick(100);
 
-      expect(spy.callCount).to.be(1);
-      expect(spy.firstCall.args[0]).to.be(100);
+      // start will immediately send the first frame, the second frame
+      // gets sent after 100ms.
+      expect(spy.callCount).to.be(2);
+      expect(spy.args[0][0]).to.be(0);
+      expect(spy.args[1][0]).to.be(100);
 
       clock.tick(900);
-      expect(spy.callCount).to.be(10);
-      //setImmediate(done);
+      expect(spy.callCount).to.be(11);
     });
   });
 });
